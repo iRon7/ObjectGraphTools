46175f522be1e68072f383bebeec7ca63e38bd7e
iRon7 <boderonald@gmail.com>
2023-11-14 15:39:42 +0100

    Add files via upload

---
+++ Tests/Merge-ObjectGraph.Tests.ps1
@@ -0,0 +1,85 @@
+#Requires -Modules @{ModuleName="Pester"; ModuleVersion="5.0.0"}
+
+Describe 'Merge-ObjectGraph' {
+
+    BeforeAll {
+
+        Set-StrictMode -Version Latest
+
+        $FileInfo = [System.Io.FileInfo]$PSCommandPath
+        $SourceFolder = Join-Path $FileInfo.Directory.Parent 'Source'
+        Foreach ($SubFolder in 'Classes', 'Private') {
+            $Folder = Join-Path $SourceFolder $SubFolder
+            $PSScripts = Get-ChildItem $Folder -Filter *.ps1
+            foreach ($PSScript in $PSScripts) { . $PSScript.FullName }
+        }
+        $PublicFolder =  Join-Path $SourceFolder Public
+        $PSScriptPath = Join-Path $PublicFolder $FileInfo.Name.Replace('.Tests.ps1', '.ps1')
+        . $PSScriptPath
+
+    }
+
+    Context 'Sanity Check' {
+
+         It 'Help' {
+             Merge-ObjectGraph -? | Out-String -Stream | Should -Contain SYNOPSIS
+         }
+    }
+
+    Context 'Basic' {
+
+        It 'Scalar Array' {
+            ,@('a', 'b', 'c') | Merge-ObjectGraph 'b', 'c', 'd' | Should -Be (,@('a','b','c','d'))
+        }
+
+        It 'Dictionary' {
+            $InputObject = @{
+                a = 1
+                b = 2
+                c = 3
+            }
+
+            $Template = @{
+                b = 2
+                c = 3
+                d = 4
+            }
+
+            $Actual = $InputObject | Merge-ObjectGraph $Template
+            $Actual.Count | Should -Be 4
+            $Actual.Keys | Should -Contain 'a'
+            $Actual.Keys | Should -Contain 'd'
+        }
+
+        It 'Dictionary Array' {
+            $InputObject = @(
+                @{
+                    Key = 'Key1'
+                    a = 1
+                    b = 2
+                }
+                @{
+                    Key = 'Key2'
+                    c = 3
+                    d = 4
+                }
+            )
+
+            $Template = @(
+                @{
+                    Key = 'Key3'
+                    a = 1
+                    b = 2
+                }
+                @{
+                    Key = 'Key2'
+                    c = 3
+                    d = 4
+                }
+            )
+
+            $Actual = ,$InputObject | Merge-ObjectGraph $Template
+            $Actual.Count | Should -Be 4
+        }
+    }
+}
