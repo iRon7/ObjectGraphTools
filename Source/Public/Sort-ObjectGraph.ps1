ac60e7c187b080466abb9dc47344b543e7876c02
iRon7 <boderonald@gmail.com>
2023-11-15 11:45:38 +0100

    Add files via upload

--- Source/Public/Sort-ObjectGraph.ps1
+++ Source/Public/Sort-ObjectGraph.ps1
@@ -1,6 +1,14 @@
-enum Construction { Undefined; Scalar; List; Dictionary; Component; Custom }
+<#
+.SYNOPSIS
+    Sort object graph
+
+.DESCRIPTION
+    Sort object graph
+    
+#>
+
 function Sort-ObjectGraph {
-    [Diagnostics.CodeAnalysis.SuppressMessage("PSUseApprovedVerbs", "")]
+    [Diagnostics.CodeAnalysis.SuppressMessage('PSUseApprovedVerbs', '')]
     [CmdletBinding()][OutputType([Object[]])] param(
 
         [Parameter(Mandatory=$true, ValueFromPipeLine = $True)]
@@ -14,18 +22,19 @@ function Sort-ObjectGraph {
     )
     begin {
         function SortObject($Object, [Int]$Depth = $MaxDepth, [Switch]$SortKey) {
-            if ($Depth -le 0) {
-                Write-Warning "The maximum depth of $MaxDepth has been reached."
-                $PSObject = [PSInterface]::new("$Object")
+            $PSObject = [PSInterface]::new($Object) 
+            if ($PSObject.Structure -eq 'Scalar' -or $Depth -le 0) {
+                if ($Depth -le 0) { Write-Warning "The maximum depth of $MaxDepth has been reached." }
+                $Key = if ($Null -eq $Object) { [Char]27 + '$Null' } elseif ($MatchCase) { "$Object".ToUpper() } else { "$Object" }
+                $Output = @{ $Key = $Object }
             }
-            else { $PSObject = [PSInterface]::new($Object) }
-            if ($PSObject.Structure -eq 'List') {
+            elseif ($PSObject.Structure -eq 'List') {
                 $Items = foreach ($Item in $Object) { SortObject $Item ($Depth - 1) -SortKey }
                 $Items = $Items | Sort-Object { $_.Keys[0] }
                 $String = [Collections.Generic.List[String]]::new()
                 $Object = [Collections.Generic.List[Object]]::new()
                 foreach ($Item in $Items) {
-                    $Key = $Item.Keys[0]
+                    $Key = $Item.get_Keys()[0]
                     $String.Add($Key)
                     $Object.Add($Item[$Key][0])
                 }
@@ -35,22 +44,22 @@ function Sort-ObjectGraph {
             elseif ($PSObject.Structure -eq 'Dictionary') {
                 $Properties = [Ordered]@{}
                 $String = [Collections.Generic.List[String]]::new()
-                $Order = $PSObject.get_Keys() | Sort-Object { if ($_ -in $PrimaryKey) { [Char]9 + "$_" } else { $_ } }
+                $Order = $PSObject.get_Keys() | Sort-Object { if ($_ -in $PrimaryKey) { [Char]27 + "$_" } else { $_ } }
                 foreach ($Index in $Order) {
                     $Item = SortObject $PSObject.Get($Index) ($Depth - 1) -SortKey
-                    $Key = $Item.Keys[0]
-                    $SortKey = if ($MatchCase) { "$Index".ToUpper() } else { "$Index" }
-                    $SortKey = $SortKey + [Char]255 + $Key
-                    $String.Add($SortKey)
+                    $Key = $Item.get_Keys()[0]
+                    $Sort =
+                        if ($MatchCase) { "$Index".ToUpper() + [Char]255 + $Key } 
+                        else { "$Index" + [Char]255 + $Key }
+                    $String.Add($Sort)
                     $Properties[$Index] = $Item[$Key][0]
                 }
                 $Name = $String -Join [Char]255
                 $Output = @{ $Name = [PSCustomObject]$Properties }                          # This will convert a dicitionary to a PSCustomObject
             }
-            else {
-                $Key = if ($MatchCase) { "$Object".ToUpper() } else { "$Object" }
-                $Output = @{ $Key = $Object }
-            }
+            else { Write-Error 'Should not happen'}
+
+            Write-Debug "$('  ' * ($MaxDepth - $Depth)) Sortkey: $($Output.get_Keys())"
             if ($SortKey) { $Output } else { $Output.get_Values() }
         }
     }
